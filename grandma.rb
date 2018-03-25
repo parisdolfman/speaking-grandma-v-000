214472c4d951ef42c19dd010b0c7c0b3edfec88d
Paris Dolfman <parisdolfman@gmail.com>
2018-03-25 20:20:25 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-     "HUH?! SPEAK UP SONNY!"
+     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
      "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
